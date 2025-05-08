54312fd3009ee95c55eaa224f1a15f23b1ce5bbb
Ayesha Hussain <127660896+AyeshaHussain475@users.noreply.github.com>
2025-05-08 19:58:22 +0500

    added middlewares

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import express from "express";
 import env from "dotenv";
 import cors from "cors";
 
-env.config();
 const app = express();
+env.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
